0b164a05bbb4c9c642c1d78b1a81b154a84d55a5
Ravi Raghavan <ravi0308raghavan@gmail.com>
2023-01-31 10:39:23 -0500

    Developed Front End for User Authentication Page. CSS needs improvement

---
+++ GoogleAuth.js
@@ -0,0 +1,3 @@
+var clientCredentials = {"web":{"client_id":"600915655715-o9ssvjgtj0aiap2q4gc8jq81pefqsmu9.apps.googleusercontent.com","project_id":"swe-spring-2023","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://oauth2.googleapis.com/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"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","redirect_uris":["http://localhost","http://localhost:8000"],"javascript_origins":["http://localhost","http://localhost:8000"]}}
+
+module.exports = {clientCredentials};
--- server.js
+++ server.js
@@ -2,11 +2,63 @@
 const http = require("http");
 const url = require("url");
 const fs = require("fs");
-const nodemailer = require('nodemailer');
+const lookup = require("mime-types").lookup;
+var google_auth =  require("./GoogleAuth").clientCredentials;
 
+function issueServerResponse(path, response){
+    console.log(`Requested Path: ${path}`);
+
+    var file = "";
+    switch(path){
+        case "/":
+            file = __dirname + "/public/user-auth-form.html";
+            break;
+        case "/user-auth-form.css":
+            file = __dirname + "/public/user-auth-form.css";
+            break;
+        case "/img/pharmacy.jpg":
+            file = __dirname + "/img/pharmacy.jpg";
+            break;
+        case "/favicon.ico":
+            file = __dirname + "/img/favicon.ico";
+            break;
+        
+        case "/GoogleAuth":
+            var responseContent = {client_id: google_auth.web.client_id}
+            response.writeHead(200, { "Content-type": "application/json" });
+            response.write(JSON.stringify(responseContent));
+            response.end();
+            break;
+            
+    }
+
+    if (file == ""){
+        // If client is not requesting a file, they are simply requesting for data. Handle that HERE
+        console.log("Respone sent");
+    }
+    else{
+        //Client is requesting a file
+        fs.readFile(file, function(err, content){
+            if (err){
+                console.log(`File Not Found ${file}`);
+                response.writeHead(404);
+                response.end()
+            }
+            else{
+                let mime = lookup(file);
+                response.writeHead(200, { "Content-type": mime });
+                response.write(content);
+                response.end();
+            }
+        })
+    }
+}
 
 const server = http.createServer((request, response) => {
     //Handle client requests and issue server response here 
+    let path = url.parse(request.url, true).path;
+    issueServerResponse(path, response);
+    
 })
 
 
