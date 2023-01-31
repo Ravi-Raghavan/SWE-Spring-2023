51cb883540e5dfc9eeb881e800aae82b55b26d04
Ravi Raghavan <ravi0308raghavan@gmail.com>
2023-01-31 10:55:00 -0500

    Added a test encryption file to test out CryptoJS encryption

---
+++ test/encryption.js
@@ -0,0 +1,12 @@
+// File to test out CryptoJS AES-based encryption for storing user's sensitive data in the application
+var CryptoJS = require("crypto-js");
+
+// Encrypt
+var key = "random_key";
+var ciphertext = CryptoJS.AES.encrypt('my message', key).toString();
+
+// Decrypt
+var bytes  = CryptoJS.AES.decrypt(ciphertext, key);
+var originalText = bytes.toString(CryptoJS.enc.Utf8);
+
+console.log(originalText); // 'random_password'
