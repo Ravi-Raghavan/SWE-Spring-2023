3bd9a7006322aea9a99569624415c93c888c6975
Ravi Raghavan <ravi0308raghavan@gmail.com>
2023-02-07 14:44:39 -0500

    Stopped SERP Api for now due to free 100 searches soon to run out

--- public/SERP.js
+++ public/SERP.js
@@ -1,11 +1,11 @@
 const SerpApi = require('google-search-results-nodejs')
-const search = new SerpApi.GoogleSearch("5aae37aa0093a0ac022d125d55ae19873e56cdd17a7f69aa110b0064d16dcafd")
+const search = new SerpApi.GoogleSearch("") //Stop SERP Api Search
 
 async function executeQuery(query){
 
     return await new Promise((resolve, rejects) => {
         search.json({
-            q: `site:www.webmd.com ${query}`, 
+            q: `site:https://www.webmd.com/drugs ${query}`, 
             location: "Austin, TX"
            }, (result) => {
                 resolve(JSON.stringify(result))
