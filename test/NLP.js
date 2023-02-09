b1c0e96cd6a29f6441c1df5dc0801d1eb2acc0d4
Ravi Raghavan <ravi0308raghavan@gmail.com>
2023-02-09 09:21:32 -0500

    Added FAQ

---
+++ test/NLP.js
@@ -0,0 +1,52 @@
+var natural = require('natural');
+var classifier = new natural.BayesClassifier();
+
+classifier.addDocument("Can I get automatic refills for my medications?", "Prescription Refill");
+classifier.addDocument("Can I obtain an early refill of my medications?", "Prescription Refill");
+classifier.addDocument("How do I order a refill for a medication I get through delivery?", "Prescription Refill");
+classifier.addDocument("Refill", "Prescription Refill");
+classifier.addDocument("How do I refill my meds?", "Prescription Refill");
+classifier.addDocument("How do I sign up for automatic refill system?", "Prescription Refill");
+classifier.addDocument("How do I enroll for automatic refill system?", "Prescription Refill");
+classifier.addDocument("What is the automatic refill system?", "Prescription Refill");
+classifier.addDocument("How do I get more medication beyond my existing prescription limit", "Prescription Refill");
+classifier.addDocument("What is the prescription limit during each automatic refill?", "Prescription Refill");
+
+
+classifier.addDocument("What is DrugHub?", "About Us");
+classifier.addDocument("Is there a mobile app for DrugHub?", "About Us");
+classifier.addDocument("Is there a website for DrugHub?", "About Us");
+
+classifier.addDocument("How do I know if I am a DrugHub member?", "Subcription Plan");
+classifier.addDocument("Who do I speak to if I have a question about my subscription plan?", "Subcription Plan");
+classifier.addDocument("Where do I find my DrugHub member ID?", "Subcription Plan");
+classifier.addDocument("How much does a Premium Membership cost?", "Subcription Plan");
+classifier.addDocument("What percentage of DrugHub users are premium subscribers?", "Subcription Plan");
+classifier.addDocument("Is the subscription plan recurring(i.e. renews automatically)?", "Subcription Plan");
+
+classifier.addDocument("How do I get started with home delivery?", "Home Delivery");
+classifier.addDocument("What medications can be delivered?", "Home Delivery");
+classifier.addDocument("How do you ship my medication?", "Home Delivery");
+classifier.addDocument("If I have my medication delivered, can I still talk to a pharmacist?", "Home Delivery");
+classifier.addDocument("Can I get all my medications delivered?", "Home Delivery");
+classifier.addDocument("Is it safe to get my medications delivered?", "Home Delivery");
+classifier.addDocument("Is there a way to get medicine shipped to my house?", "Home Delivery");
+classifier.addDocument("Is there a way to get medicine sent to my house?", "Home Delivery");
+classifier.addDocument("How is medication sent to my home?", "Home Delivery");
+classifier.addDocument("Who delivers the medication to my home?", "Home Delivery");
+classifier.addDocument("Why is home delivery a good option?", "Home Delivery");
+
+classifier.addDocument("How do I contact DrugHub?", "Contact Us");
+classifier.addDocument("Who do I call if I have additional questions or need help?", "Contact Us");
+classifier.addDocument("Are there representatives I can speak to?", "Contact Us");
+
+
+classifier.train();
+
+question = "Is there a way for me to get medication without going directly to a pharmacy?";
+console.log(classifier.classify(question));
+console.log(natural.PorterStemmer.tokenizeAndStem(question));
+
+article = "Avoid going to a pharmacy with home delivery!";
+
+console.log(natural.PorterStemmer.tokenizeAndStem(article));
