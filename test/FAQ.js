216c59840edbc14df8745c664698907eabcb6a0e
Ravi Raghavan <ravi0308raghavan@gmail.com>
2023-02-14 14:32:21 -0500

    Working on FAQ

--- test/FAQ.js
+++ test/FAQ.js
@@ -1,3 +1,15 @@
 //test file to create a bunch of dummy FAQ files and store them in firebase 
+const admin = require("../firebase").admin;
+var db = admin.database();
+var ref = db.ref("/FAQ/");
 
+
+
+
+// var uid = "article_1";
+// ref.child(`${uid}`).set({
+//     title: "Who is Jeff Acevedo?",
+//     snippet: "",
+//     category: ''
+// })
 
