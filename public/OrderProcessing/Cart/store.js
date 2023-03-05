bcefa07ccee2370a1ca5ababaec234bb7293dd46
QadisChaudhry <qadis.chaudhry@gmail.com>
2023-03-05 17:27:15 -0500

    cleanup html

--- public/OrderProcessing/Cart/store.js
+++ public/OrderProcessing/Cart/store.js
@@ -59,9 +59,9 @@ document.getElementsByClassName('btn-purchase')[0].addEventListener('click', pur
 }
 
 function purchaseClicked() {
-    alert('Thank you for your purchase')                                // Insert HTTP request here to transfer to payment page.
+    // alert('Thank you for your purchase')                                // Insert HTTP request here to transfer to payment page.
                                                                         // Each time an order is changed the product is added to User/Orders/ in Firebase
-                                                                        
+
     var cartItems = document.getElementsByClassName('cart-items')[0]
     while (cartItems.hasChildNodes()) {
         cartItems.removeChild(cartItems.firstChild)
@@ -95,11 +95,11 @@ function addToCartClicked(event) {
     console.log(title, price)
     //Add item to order JSON.
     order.push({"item-name" : title, "price" : priceVal , "quantity" : 1})
-    console.log(order)  
+    console.log(order)
 
     addItemToCart(title, price,imageSrc)
     updateCartTotal()
-}   
+}
 
 function addItemToCart(title, price, imageSrc) {
     var cartRow = document.createElement('div')
@@ -107,12 +107,12 @@ function addItemToCart(title, price, imageSrc) {
     cartRow.innerText = title
     var cartItems = document.getElementsByClassName('cart-items')[0]
     var cartItemNames = cartItems.getElementsByClassName('cart-item-title')
-    
+
     for (var i = 0; i < cartItemNames.length; i++)
     {
         if(cartItemNames[i].innerText == title) {
             alert('This item is already added to the cart')
-            return 
+            return
         }
     }
 
@@ -132,7 +132,7 @@ cartRow.innerHTML = cartRowContents
     cartItems.append(cartRow)
     cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem)
     cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged)
-}   
+}
 
 
 function updateCartTotal() {
