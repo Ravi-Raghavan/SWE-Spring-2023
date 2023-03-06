7ba1963ae91c25fe09615e399e21258777cbe98c
Abillou <abillou59@gmail.com>
2023-03-06 09:41:14 -0500

    Fixed Issue with Prescription Page -Abid

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const GoogleAuth = require("./public/GoogleAuth")
 const firebaseAPI = require("./public/FirebaseAPI");
 const public_paths = ["/homepage.html", "/homepage.css", "/homepage-nav.css", "/homepage-footer.css", "/user-registration-form.html",  "/user-registration-form.css", "/user-auth-form.css", "/dashboard.css",
                     "/waiting-for-validation.html", "/waiting-for-validation.css", "/email-validated.css", "/user-auth-form.html", "/email-validated.html", "/SMTP.js",
-                    "/knowledge-base.html", "/knowledge-base.css", "/FAQ.html", "/FAQ.css", "/PrescriptionRequest.html", "/logoutPage.html", "/darkpan-1.0.0/index.html", "/OrderProcessing/Cart/store.html", "/dashboard.html",
+                    "/knowledge-base.html", "/knowledge-base.css", "/FAQ.html", "/FAQ.css", "/PrescriptionRequest.html", "/Prescription.css", "/logoutPage.html", "/darkpan-1.0.0/index.html", "/OrderProcessing/Cart/store.html", "/dashboard.html",
                     "/OrderProcessing/Cart/store.js", "/OrderProcessing/Cart/style.css", "/dynamic-size.css", "/contact-us.html", "/contact-us.css", "/deliverypage.html", "/deliverypage.css"]
 const img_paths = ["/img/protien_powder_2.png", "/img/top_logo.png", "/img/tablets.png", "/img/tablets_3.png", "/img/tablets_2.png", "/img/protein_powder.png",
                     "/img/protein_powder_2.png", "/img/mayank_profile.png", "/img/logo.png", "/img/jeff_profile.png", "/img/insulin_meter.png", "/img/bottom_logo.png",
