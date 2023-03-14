b846f75c986f27d97197f3d2c552a4eb6bd18560
Nikhil Agarwal <email.nikhil.agarwal@gmail.com>
2023-03-14 05:13:30 -0400

    doctor prescription form & functionality

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const public_paths_html = [
   "/html/user-registration-form.html",
   "/html/waiting-for-validation.html",
   "/html/store.html",
-  "/html/submitted-prescription.html",
+  "/html/submitted-prescription-patient.html",
 ];
 
 const public_paths_css = [
