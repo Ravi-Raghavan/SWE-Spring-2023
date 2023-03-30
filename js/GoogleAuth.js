1f64429de6179566040ed3042713c2c4f6c5cb48
Ravi Raghavan <ravi0308raghavan@gmail.com>
2023-03-30 15:38:11 -0400

    Added Functionality to avoid users creating duplicate admin accounts

--- js/GoogleAuth.js
+++ js/GoogleAuth.js
@@ -28,6 +28,13 @@ function register(request, response){
         var email = decryptedToken.email;
         var accountType = credentials.accountType;
 
+        if (accountType == "Admin" && email != "swespring2023@gmail.com"){
+            response.writeHead(404, { "Content-type": "text/plain" });
+            response.write(`Cannot create another admin account!`);
+            response.end();
+            return;
+        }
+
         var userParameters = {accountType: accountType, email: email, emailVerified: true, password: "Google-OAuth", displayName: decryptedToken.name, photoURL: decryptedToken.picture, disabled: false}
         await firebaseAPI.register(userParameters, response);
     })
